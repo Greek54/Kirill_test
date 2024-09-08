e8e576bfde09cd6dd0c24a0577b20b645a8522d2
Kirill <greek.nsk@gmail.com>
2024-09-09 01:08:26 +0700

    my first commit

---
+++ core/contracts.py
@@ -0,0 +1,11 @@
+USER_DATA_SCHEME = {
+    "type": "objects",
+    "properties": {
+    "id": {"type": "number"},
+    "email": {"type": "string"},
+    "first_name": {"type": "string"},
+    "last_name": {"type": "string"},
+    "avatar": {"type": "string" }
+},
+"required":["id", "mail", "first_name", "last_name", "avatar"]
+}
